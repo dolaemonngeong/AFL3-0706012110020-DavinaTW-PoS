f6f13b29edcce86179576727178e564dc73ada4c
Davina T W <91788095+dolaemonngeong@users.noreply.github.com>
2023-04-25 04:06:11 +0700

    Initial Commit

---
+++ AFL3-0706012110020-DavinaTW-PoS/AFL3_0706012110020_DavinaTW_PoSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110020_DavinaTW_PoSApp.swift
+//  AFL3-0706012110020-DavinaTW-PoS
+//
+//  Created by MacBook Pro on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110020_DavinaTW_PoSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
